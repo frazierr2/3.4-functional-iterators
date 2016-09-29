0279984f6cab8a79bc8af6919f1e309fcbc8e304
Ryan Frazier <frazierr2@gmail.com>
2016-09-29 16:17:25 -0400

    Question 2

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
+(function () {
+  "use strict";
+
+
 //***********QUESTION 1************
 var sum = items.reduce(function(total, num){ return total + num.price }
   , 0);
 
-console.log('The average price is '+ '$'+ (sum/25).toFixed(2));
+console.log('The average price is '+ '$'+ (sum/items.length).toFixed(2));
 
 
 //***********QUESTION 2*****************
+var newArray = items.filter(function(num){
+  return num.price > 14 &&  num.price < 18});
+console.log(newArray);
+
+
+//********QUESTION 3******************
+var money = items.map(function(money){
+  console.log(money.currency_code);
+});
+
+
+
+
+
+
+
+
+
+}());
