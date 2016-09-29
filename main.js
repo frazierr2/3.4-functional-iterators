fb19fcf39a30a759e940f67b79a141f1b8b1bffc
Ryan Frazier <frazierr2@gmail.com>
2016-09-29 14:32:48 -0400

    Question 1 commit

---
+++ main.js
@@ -0,0 +1,8 @@
+//***********QUESTION 1************
+var sum = items.reduce(function(total, num){ return total + num.price }
+  , 0);
+
+console.log('The average price is '+ '$'+ (sum/25).toFixed(2));
+
+
+//***********QUESTION 2*****************
